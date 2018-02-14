bab0f4955b2eda0edf5ed9a32e1934586bb01c59
ShiHua <ShiHua@huaguangmeideMacBook-Pro.local>
2018-02-14 17:26:32 +0800

    获取访问者的IP地址

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress });
 });
 
 module.exports = router;
